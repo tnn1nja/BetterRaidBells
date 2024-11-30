ff56bf5e87439e233a1cf06182efb0c72cb04776
tnn1nja <thenonsenseninja@gmail.com>
2024-11-30 01:22:24 +0000

    Added .gitignore and setup basics

--- src/main/java/net/tnn1nja/betterRaidBells/BetterRaidBells.java
+++ src/main/java/net/tnn1nja/betterRaidBells/BetterRaidBells.java
@@ -1,17 +1,40 @@
 package net.tnn1nja.betterRaidBells;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BellResonateEvent;
+import org.bukkit.event.block.BellRingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BetterRaidBells extends JavaPlugin {
+import java.util.Collection;
+import java.util.logging.Logger;
+
+public final class BetterRaidBells extends JavaPlugin implements Listener {
+
+    Logger log = Bukkit.getLogger();
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+        log.info("[BetterRaidBells] BetterRaidBells loaded.");
+    }
 
+    @EventHandler
+    public void onBellRing(BellRingEvent e){
+        Location l = e.getBlock().getLocation();
+        Collection<Entity> entities = l.getWorld().getNearbyEntities(l, 128, 128, 128);
+    }
+
+    @EventHandler
+    public void onBellResonate(BellResonateEvent e){
+        e.getResonatedEntities().clear();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        log.info("[BetterRaidBells] BetterRaidBells loaded.");
     }
 }
