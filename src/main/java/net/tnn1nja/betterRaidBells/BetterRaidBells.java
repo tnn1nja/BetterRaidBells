df175b425d59ac0881a3432c6d32557452fc6564
tnn1nja <thenonsenseninja@gmail.com>
2024-12-01 16:39:03 +0000

    Refactored, sphericalised custom radius, implemented custom version of bell resonation event

--- src/main/java/net/tnn1nja/betterRaidBells/BetterRaidBells.java
+++ src/main/java/net/tnn1nja/betterRaidBells/BetterRaidBells.java
@@ -2,22 +2,24 @@ package net.tnn1nja.betterRaidBells;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Sound;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.Raider;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BellResonateEvent;
 import org.bukkit.event.block.BellRingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.logging.Logger;
 
 public final class BetterRaidBells extends JavaPlugin implements Listener {
 
-    //Constants
     Logger log = Bukkit.getLogger();
     int vanillaDetectionRadius = 32;
     int customDetectionRadius = 128;
@@ -35,44 +37,59 @@ public final class BetterRaidBells extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onBellRing(BellRingEvent e){
-        //Determine if Vanilla Resonate Event Fires
         Location l = e.getBlock().getLocation();
-        Collection<Entity> entities =
-                l.getWorld().getNearbyEntities(l,
-                        vanillaDetectionRadius, vanillaDetectionRadius, vanillaDetectionRadius,
-                        entity -> entity instanceof Raider && isWithinVanillaRange(entity.getLocation(), l));
-
-        if(!entities.isEmpty()){
-            Collection<LivingEntity> raidersInRange = new ArrayList<LivingEntity>();
-            for(LivingEntity le: getRaidersWithinCustomRange(l)){
-                raidersInRange.add(le);
-            }
+        if(!isRaidersWithinVanillaRange(l)){
+            Bukkit.getScheduler().runTaskLater(this, new Runnable() {
+                @Override
+                public void run() {
+                    for(LivingEntity le: getRaidersWithinCustomRange(l)){
+                        le.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING, 60, 0));
+                    }
+                }
+            }, 5L);
+            Bukkit.getScheduler().runTaskLater(this, new Runnable() {
+                @Override
+                public void run() {
+                    for(Player p: Bukkit.getOnlinePlayers()){
+                        p.playSound(l, Sound.BLOCK_BELL_RESONATE, 1, 1);
+                    }
+                }
+            }, 3L);
+            log.info("[BetterRaidBells] Bell resonated with range of " + customDetectionRadius + " blocks.");
         }
     }
 
     @EventHandler
     public void onBellResonate(BellResonateEvent e){
-        //Add All Pillagers in Custom Range to Default Resonate Event
         Location l = e.getBlock().getLocation();
         e.getResonatedEntities().addAll(getRaidersWithinCustomRange(l));
+        log.info("[BetterRaidBells] Bell resonation range extended to " + customDetectionRadius + " blocks.");
     }
 
+    public boolean isRaidersWithinVanillaRange(Location bellLocation){
+        Collection<Entity> entities = bellLocation.getWorld().getNearbyEntities(bellLocation,
+                        vanillaDetectionRadius, vanillaDetectionRadius, vanillaDetectionRadius);
+        for(Entity e: entities){
+            if(e instanceof Raider && isWithinSphere(e.getLocation(), bellLocation, vanillaDetectionRadius)){
+                return true;
+            }
+        }
+        return false;
+    }
 
-
-    //Find if Entity Coordinate is Within Radius of Bell
-    public boolean isWithinVanillaRange(Location entityLocation, Location bellLocation){
+    public boolean isWithinSphere(Location entityLocation, Location bellLocation, int radius){
         return Math.pow((bellLocation.getX() - entityLocation.getX()), 2) +
                 Math.pow((bellLocation.getY() - entityLocation.getY()), 2) +
                 Math.pow((bellLocation.getZ() - entityLocation.getZ()), 2)
-                <= Math.pow(vanillaDetectionRadius, 2);
+                <= Math.pow(radius, 2);
     }
 
-    //Returns all Raiders within a 128 Box of Location
-    public Collection<LivingEntity> getRaidersWithinCustomRange(Location l){
-        return l.getWorld().getNearbyEntities(l,
-                        customDetectionRadius, customDetectionRadius, customDetectionRadius,
-                        entity -> entity instanceof Raider)
-                .stream().map(entity -> (LivingEntity) entity).toList();
+    public Collection<LivingEntity> getRaidersWithinCustomRange(Location bellLocation){
+        return bellLocation.getWorld().getNearbyEntities(bellLocation,
+                customDetectionRadius, customDetectionRadius, customDetectionRadius,
+                entity -> entity instanceof Raider &&
+                        isWithinSphere(entity.getLocation(), bellLocation, customDetectionRadius)).
+                stream().map(entity -> (LivingEntity) entity).toList();
     }
 
 }
